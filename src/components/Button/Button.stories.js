9905d50a00520c66f6048ca39e8d6f649b5a3020
arwa.lokhandwala <arwa18793@gmail.com>
2019-10-19 17:53:28 +0800

    Integration testing using specs addon

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
+import { specs, describe, it } from 'storybook-addon-specifications'
+
+import {mount} from "enzyme";
+import expect from "expect";
+
 import { Button } from './';
 
 export const view = {
@@ -14,5 +19,15 @@ export const actions = {
 };
 
 storiesOf('Button', module)
-    .add('default', () => <Button view={{ ...view }} {...actions} />)
+    .add('default', () => {
+    const story = <Button view={{ ...view }} {...actions} />
+
+    specs(() => describe('Button', function() {
+        it('Should be in default state', function(){
+            let output = mount(story);
+            expect(output.props().disabled).to.equal(true);
+        })
+    }))
+    return story
+    })
     .add('active', () => <Button view={{ ...view, disabled: false }} {...actions} />)
